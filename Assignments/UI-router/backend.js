00e1d93a8e328434a190deda08329e04c1b84c44
praneethpannala <praneeth.pannala@gmail.com>
2016-12-06 16:03:09 +0530

    Project using UI-route

--- Assignments/UI-router/backend.js
+++ Assignments/UI-router/backend.js
@@ -1,16 +1,78 @@
-var myApp = angular.module('myApp', ['ui.router']);
+(function() {
 
-myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+"use strict";
+var app = angular.module('myApp', ['ui.router']);
+
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
  
-  	$urlRouterProvider.otherwise('/');
+  	$urlRouterProvider.otherwise('/home');
 
 	$stateProvider
 
-    .state('party', {
-        url: '/party',
-        template: '<h1>This Is A State</h1>'
+    .state('home', {
+        url: '/home',
+        templateUrl: 'home.html',
+        controller: 'info'
+    })
+
+    .state('education', {
+        url: '/education',
+        templateUrl: 'education.html',
+        controller: 'info'
+    })
+
+	.state('skills', {
+	        url: '/skills',
+	        templateUrl: 'skills.html',
+          controller: 'info'
+   })
+	.state('interests', {
+	        url: '/interests',
+	        templateUrl: 'interests.html',
+            controller: 'info'
     });
- }]);   
+
+ }]); 
+
+app.controller('info',function(myService,$scope,$rootScope)
+	{
+		$rootScope.data="";
+		console.log(this);
+		myService.myfunc(myFun,"myjson.json");
+		$scope.home="";
+		function myFun(data)
+		{		
+			console.log(data);	
+			$rootScope.data= data.home;
+			$scope.home= data.home;
+			console.log($scope.home);
+		}	
+
+		$scope.education= $rootScope.education;
+		$scope.skills- $rootScope.skills;
+		$scope.interests= $rootScope.interests;
+
+	});	
+
+app.service('myService',function($http)
+	{
+		this.myfunc= function(callBack,x)
+		{
+			return $http.get(x)
+  				.then(function(response) {
+  					callBack(response.data);	
+  				});
+		}
+
+	});
 
 
-myApp.controller()
+app.directive('navBar',function(){
+	return{
+		restrict: 'E',
+		templateUrl: 'navbar.html'
+	}
+});
+
+
+})();
