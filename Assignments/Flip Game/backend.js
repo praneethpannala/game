376da48258878cade0db40953264f5b9c3893bc3
praneethpannala <praneeth.pannala@gmail.com>
2016-12-15 18:04:09 +0530

    Game on flipping

--- Assignments/Flip Game/backend.js
+++ Assignments/Flip Game/backend.js
@@ -1,5 +1,31 @@
 var app= angular.module('myApp',[]);
 
-app.controller('myCtrl',function(){
-	console.log("hidacb");
+app.controller('myCtrl',function($scope)
+{
+		
+});
+
+app.directive('actualdir',function(){
+	return {
+
+		controller: function($scope, $element, $attrs){
+
+			 var self = this;	
+			hey()
+			{
+				console.log("hsb")
+			}
+
+			self.init = function()
+			{
+				element.bind('click',hey)
+		    }
+
+
+
+		},	
+		 link : function(scope,element,attrs, ctrl){
+		 	ctrl.init();
+		 }
+	}
 });
