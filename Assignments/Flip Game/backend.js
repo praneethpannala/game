b17819072cc84d5e135e7b63f583f30cb8cb183f
praneethpannala <praneeth.pannala@gmail.com>
2016-12-19 18:40:28 +0530

    Flip Gamw with imporved CSS

--- Assignments/Flip Game/backend.js
+++ Assignments/Flip Game/backend.js
@@ -1,7 +1,114 @@
 var app= angular.module('myApp',[]);
 
+app.controller('myCtrl',function($scope,$window,$interval,$timeout) {
+
+	 $scope.list = [
+  {
+   "picture": "google.png",
+   "value": 1,
+   "id":1
+  },
+  {
+   "picture": "starbucks.png",
+   "value": 2,
+    "id":1
+
+  },
+  {
+   "picture": "puma.jpg",
+   "value": 3,
+    "id":1
+
+  },
+  {
+   "picture": "mozilla.png",
+   "value": 4,
+    "id":1
+
+  },
+  {
+   "picture": "hp.jpg",
+   "value": 5,
+    "id":1
+
+  },
+  {
+   "picture": "fanta.jpg",
+   "value": 6,
+    "id":1
+
+  },
+  {
+   "picture": "google.png",
+   "value": 1,
+    "id":1
+
+  },
+  {
+   "picture": "starbucks.png",
+   "value": 2,
+    "id":1
+
+  },
+  {
+   "picture": "puma.jpg",
+   "value": 3,
+    "id":1
+
+  },
+  {
+   "picture": "mozilla.png",
+   "value": 4,
+    "id":1
+
+  },
+  {
+   "picture": "hp.jpg",
+   "value": 5,
+    "id":1
+
+  },
+  {
+   "picture": "fanta.jpg",
+   "value": 6,
+    "id":1
+
+  }];
+
+  $scope.reloadRoute = function() {
+
+   $window.location.reload();
+   
+	}
+
+    //generic shuffling function
+    $scope.shuffle = function(o){ 
+        for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+        return o;
+    };
+
+
+    $scope.countDown = 60; 
+    var timer= $interval(function(){
+    	$scope.countDown--; 
+    	console.log($scope.countDown);
+    	},1000);
+
+    $timeout(function() 
+	{
+		console.log("sbd");
+		$interval.cancel(timer);
+	
+	},10000);
+ 
+
+});
+
+
 app.directive('actualdir',function(){
 	return {
+		restrict:'EA',
+		scope:false,
 
 		controller: function($scope, $element, $attrs,$timeout){
 			 var self = this;
@@ -27,23 +134,32 @@ app.directive('actualdir',function(){
       		
 			function flip(x,z)
 			{
+				 console.log("counter=" +counter);
+
 				
 				if (counter == 0) 
   				{
   					box1= x;
   					div1 = angular.element(box1).parent().children();
+  					console.log("counter=" +counter);
   					firstDiv= z;
+  					console.log("value="+z);
+ 					console.log(x);
+
   					showFront(box1,div1[1]);
   					counter++;
   				}
   				else if(counter==1)
   				{
+  					console.log("counter=" +counter);
+					console.log(z);
+ 					console.log(x);
+
   					box2=x;
   					div2 = angular.element(box2).parent().children();
-  					secondDiv= z;
+  					secondDiv=  z;
   					showFront(box2,div2[1]);
-  					
-  						korakala(firstDiv,secondDiv);	
+  					korakala(firstDiv,secondDiv);	
   				}
 
 				function korakala(firstDiv,secondDiv) 
@@ -63,9 +179,9 @@ app.directive('actualdir',function(){
 			self.init = function()
 			{
 		        $scope.ele= function(a){
-		        	console.log(a.target);
-
-			        flip(a.target,a.currentTarget.getAttribute("value"));
+		        	var x = a.currentTarget;
+		        	var z = angular.element(x).parent()[0].getAttribute('value');
+			        flip(x, z);
 		        }  
 		    }
 
