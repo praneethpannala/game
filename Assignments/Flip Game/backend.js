ad12c0494095e35f3331accd31a5f426eadb968a
praneethpannala <praneeth.pannala@gmail.com>
2016-12-20 18:13:47 +0530

    Flip Game

--- Assignments/Flip Game/backend.js
+++ Assignments/Flip Game/backend.js
@@ -1,121 +1,88 @@
 var app= angular.module('myApp',[]);
 
-app.controller('myCtrl',function($scope,$window,$interval,$timeout) {
 
-	 $scope.list = [
-  {
-   "picture": "google.png",
-   "value": 1,
-   "id":1
-  },
-  {
-   "picture": "starbucks.png",
-   "value": 2,
-    "id":1
+app.controller('myCtrl',function($scope,$window,$interval,myService) {
 
-  },
-  {
-   "picture": "puma.jpg",
-   "value": 3,
-    "id":1
+	 $scope.list = "";
 
-  },
-  {
-   "picture": "mozilla.png",
-   "value": 4,
-    "id":1
-
-  },
-  {
-   "picture": "hp.jpg",
-   "value": 5,
-    "id":1
-
-  },
-  {
-   "picture": "fanta.jpg",
-   "value": 6,
-    "id":1
-
-  },
-  {
-   "picture": "google.png",
-   "value": 1,
-    "id":1
-
-  },
-  {
-   "picture": "starbucks.png",
-   "value": 2,
-    "id":1
-
-  },
-  {
-   "picture": "puma.jpg",
-   "value": 3,
-    "id":1
-
-  },
-  {
-   "picture": "mozilla.png",
-   "value": 4,
-    "id":1
-
-  },
-  {
-   "picture": "hp.jpg",
-   "value": 5,
-    "id":1
-
-  },
-  {
-   "picture": "fanta.jpg",
-   "value": 6,
-    "id":1
-
-  }];
-
-  $scope.reloadRoute = function() {
-
-   $window.location.reload();
-   
-	}
-
-    //generic shuffling function
-    $scope.shuffle = function(o){ 
-        for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-        return o;
-    };
-
-
-    $scope.countDown = 60; 
-    var timer= $interval(function(){
-    	$scope.countDown--; 
-    	console.log($scope.countDown);
-    	},1000);
-
-    $timeout(function() 
-	{
-		console.log("sbd");
-		$interval.cancel(timer);
+	 $scope.start= function(){
+		 //timer for the game
+	    $scope.countDown = 60; 
+	    var timer= $interval(function()
+	    	{
+		    	$scope.countDown--; 
+		    	console.log($scope.countDown);
+		    	counting();
+		    },1000);
+	    // checking when the time finishes
+	    function counting() 
+	    {
+		   	if ($scope.countDown==0) 
+		   	{	
+				console.log("sbd");
+				$interval.cancel(timer);
+		   	}
+	    }
 	
-	},10000);
+	 };
+
+	myService.mySerFun("data.json").then(
+		function(result){
+			$scope.list=myService.shuffle(result);;
+		}, function(error){
+			console.log(error.statusText);
+		});
+
+	$(document).ready(function () 
+	{
+		$('#startgame').modal('show');
+	});
  
 
 });
 
+
+app.service('myService',function($http,$q) {
+	this.mySerFun= function(x){
+		return $http.get(x).then(
+			function(response){
+				return $q.resolve(response.data);
+			}, 
+			function(response){
+				return $q.reject(response.data);
+			})
+		};
+
+
+	this.shuffle = function (array) {
+          var arrayCopy = angular.copy(array);
+          var len = arrayCopy.length;
+          var newArray = [];
+          while(len > 0){
+            var i = Math.floor(Math.random() * len);
+            newArray.push(arrayCopy[i]);
+            arrayCopy.splice(i,1);
+            len--;
+          }
+          return newArray;
+        }			
+
+
+});
 
 app.directive('actualdir',function(){
 	return {
 		restrict:'EA',
 		scope:false,
 
-		controller: function($scope, $element, $attrs,$timeout){
+		controller: function($scope, $element, $attrs,$timeout,$window){
+			$scope.hey="namaste";
 			 var self = this;
 			 var counter= 0;
+			 $scope.reducetime=60;
 			 var firstDiv,secondDiv="";
 			 var div1,div2,box1,box2;
-      	
+			 var successFlips=0;      	
 
       		function showFront(x,y) {
       			angular.element(x).addClass("myObj");
@@ -130,31 +97,21 @@ app.directive('actualdir',function(){
 				angular.element(y).addClass("normal");
 				angular.element(y).removeClass("myObj");
       		}
-      		
+
       		
 			function flip(x,z)
 			{
-				 console.log("counter=" +counter);
-
-				
 				if (counter == 0) 
   				{
   					box1= x;
   					div1 = angular.element(box1).parent().children();
-  					console.log("counter=" +counter);
   					firstDiv= z;
-  					console.log("value="+z);
- 					console.log(x);
-
+  					
   					showFront(box1,div1[1]);
   					counter++;
   				}
   				else if(counter==1)
   				{
-  					console.log("counter=" +counter);
-					console.log(z);
- 					console.log(x);
-
   					box2=x;
   					div2 = angular.element(box2).parent().children();
   					secondDiv=  z;
@@ -167,11 +124,53 @@ app.directive('actualdir',function(){
 					counter=0;
 					$timeout(function() {
 
-					if (firstDiv != secondDiv)
-					{	showBack(div1[1],box1);
-						showBack(div2[1],box2);
-							
-					} }, 500);
+						if (firstDiv != secondDiv)
+						{	showBack(div1[1],box1);
+							showBack(div2[1],box2);
+								
+						}
+						else {
+							successFlips++;
+							result();
+						}  
+					}, 500);
+				}
+
+
+				 function output(){
+				 //timer for the game
+				    var timer= $interval(function()
+				    	{
+					    	$scope.reducetime--; 
+					    	console.log($scope.reducetime);
+					    	counting();
+					    	result();
+					    },1000);
+				    // checking when the time finishes
+				    function counting() 
+				    {
+					   	if ($scope.reducetime==0) 
+					   	{
+
+							console.log("sbd");
+							$interval.cancel(timer);
+							alert("You lost the game");
+							$window.location.reload();
+					   	}
+				    }
+				};
+
+				function result() 
+				{
+					if (successFlips == 6 && $scope.reducetime>0)
+					{
+						 	alert("You won");
+					}
+					else
+					{
+						alert("You lost");
+					 	$window.location.reload();
+					 }	
 				}
 			
 			}
@@ -182,7 +181,9 @@ app.directive('actualdir',function(){
 		        	var x = a.currentTarget;
 		        	var z = angular.element(x).parent()[0].getAttribute('value');
 			        flip(x, z);
-		        }  
+			        
+		        } 
+		         
 		    }
 
 		},	
