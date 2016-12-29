bca59adf0b73f5a36aece245f38a8271c392ac1f
praneethpannala <praneeth.pannala@gmail.com>
2016-12-29 18:29:08 +0530

    Assignment Completion

--- Assignment2/js/myGraphDirective.js
+++ Assignment2/js/myGraphDirective.js
@@ -1,34 +1,34 @@
-( function(){
+(function() {
 
     "use strict";
 
     //directive for the navbar
     var app = angular.module("app");
-app.directive('navbar', function() {
-    return {
-        restrict: 'EA',
-        scope: false,
-        templateUrl: "./views/navbar.html",
-     }
-});
+    app.directive('navbar', function() {
+        return {
+            restrict: 'EA',
+            scope: false,
+            templateUrl: "./views/navbar.html",
+        }
+    });
 
-// directive for the graphs
-app.directive('graphs', function() {
-    return {
-        restrict: 'EA',
-        scope: false,
-        templateUrl: "./views/graphs.html",
-     }
-});
+    // directive for the graphs
+    app.directive('graphs', function() {
+        return {
+            restrict: 'EA',
+            scope: false,
+            templateUrl: "./views/graphs.html",
+        }
+    });
 
-// directive for the tabularCol
-app.directive('tabularcol', function() {
-    return {
-        restrict: 'EA',
-        scope: false,
-        templateUrl: "./views/tabularCol.html",
-     }
-});
+    // directive for the tabularCol
+    app.directive('tabularcol', function() {
+        return {
+            restrict: 'EA',
+            scope: false,
+            templateUrl: "./views/tabularCol.html",
+        }
+    });
 
 
 })();
